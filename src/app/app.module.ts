f12df09377c439cd756a8394bb455d9b20c553cc
Samuel Ackermann <saemi.ackermann@gmail.com>
2018-11-12 19:04:34 +0100

    implement simple sidebar and toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,6 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
     AppRoutingModule,
     SharedModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule} from '@angular/router';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatIconModule,
     RouterModule
   ],
   exports: [
+    MatButtonModule,
+    MatIconModule,
     RouterModule
   ]
 })
