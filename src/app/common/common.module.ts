9178808a5580c81e452caedf6779108d77cba1a5
Samuel Ackermann <saemi.ackermann@gmail.com>
2018-11-12 21:08:56 +0100

    create CommonModule

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HomePageComponent } from './components/home/home-page.component';
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: []
+})
+export class CommonModule { }
---
+++ src/app/common/components/home/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
