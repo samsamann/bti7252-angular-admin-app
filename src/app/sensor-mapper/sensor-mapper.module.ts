849d34764290bf78e211206e9fe35a2b9b194893
Samuel Ackermann <saemi.ackermann@gmail.com>
2018-11-23 12:49:21 +0100

    implement sensor mapper card view

--- src/app/sensor-mapper/sensor-mapper.module.ts
+++ src/app/sensor-mapper/sensor-mapper.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import { SensorMapperRoutingModule } from './sensor-mapper-routing.module';
+import {SharedModule} from '../shared/shared.module';
 
 import { MapperPageComponent } from './components/mapper-page/mapper-page.component';
+import { SensorCardComponent } from './components/sensor-card/sensor-card.component';
+import { SensorMapperDesktopComponent } from './components/sensor-mapper-desktop/sensor-mapper-desktop.component';
+
 
 @NgModule({
   declarations: [
-    MapperPageComponent
+    MapperPageComponent,
+    SensorCardComponent,
+    SensorMapperDesktopComponent
   ],
   imports: [
-    SensorMapperRoutingModule
+    CommonModule,
+    SensorMapperRoutingModule,
+    SharedModule
   ]
 })
 export class SensorMapperModule { }
