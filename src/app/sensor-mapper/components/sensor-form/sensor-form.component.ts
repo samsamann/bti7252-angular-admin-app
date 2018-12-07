5356ee873f1a5e5eb516c0fff1805d0478c0a504
Huser Emanuel, ENT-BIZ-SSO-APO-AIT <Emanuel.Huser@swisscom.com>
2018-12-07 15:11:10 +0100

    Start with get request to sample service

--- src/app/sensor-mapper/components/sensor-form/sensor-form.component.ts
+++ src/app/sensor-mapper/components/sensor-form/sensor-form.component.ts
@@ -14,6 +14,7 @@ export class SensorFormComponent implements OnInit {
 
   saveForm() {
     console.log("save");
+   // this.sensorService.getSensors();
   }
 
   cancelForm() {
--- src/app/sensor-mapper/services/sensor.service.ts
+++ src/app/sensor-mapper/services/sensor.service.ts
@@ -1,11 +1,43 @@
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams, HttpRequest} from "@angular/common/http";
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { tap, map } from 'rxjs/operators';
 
 import { SensorMapperModule } from '../sensor-mapper.module';
+
+interface Sensor {
+  id: string;
+  name:string;
+  info:string;
+}
 
 @Injectable({
   providedIn: SensorMapperModule
 })
 export class SensorService {
 
-  constructor() { }
+  courses: Observable<Sensor[]>;
+
+  host: String = "https://angular-http-guide.firebaseio.com"
+
+  constructor(private http:HttpClient) { }
+
+  getSensors() : [Sensor] {
+
+    const params = new HttpParams()
+           .set('orderBy', '"$key"')
+           .set('limitToFirst', "1");
+
+    const headers = new HttpHeaders().set("X-CustomHeader", "custom header value");
+
+    this.courses = this.http
+        .get(
+          this.host + "/courses.json",
+          {params: params}).pipe(tap(console.log));
+        //.map(data => _.values(data));
+        //https://stackoverflow.com/questions/50209119/property-do-does-not-exist-on-type-observableiproduct
+
+        return [{id:"",name:"",info:""}]
+  }
+
 }
